5e77a8b4840fd4b2ec1bcd5a80af65e10973d04e
Shawn Lautebach <slautebach@gmail.com>
2023-05-29 09:24:24 -0400

    fix indenting syntax error

--- BuildScripts/PS-Modules/Functions.psm1
+++ BuildScripts/PS-Modules/Functions.psm1
@@ -285,7 +285,7 @@ function Format-XML ([xml]$xml, $indent=2)
 {
     $StringWriter = New-Object System.IO.StringWriter
     $XmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter
-    $xmlWriter.Formatting = “indented”
+    $xmlWriter.Formatting = "indented"
     $xmlWriter.Indentation = $Indent
     $xml.WriteContentTo($XmlWriter)
     $XmlWriter.Flush()
